32ee4e1b43001ed26873f9c1fba83b361241088b
Michael Laszloffy <michael.laszloffy@oicr.on.ca>
2020-05-21 15:10:55 -0400

    Test metrics: md5sum reads, not gzip

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -2,7 +2,9 @@
 
 ls -1
 
-find . -type f -iname "*.fastq.gz" -exec md5sum {} \;
+# gzipped fastqs with timestamp - can't directly md5sum fastq.gz then...
+# find . -type f -iname "*.fastq.gz" -exec zcat {} | md5sum \;
+find . -type f -iname "*.fastq.gz" -printf "%f " -exec /bin/bash -c "zcat {} | md5sum | cut -d ' ' -f 1" \;
 
 module load samtools/1.9 2>/dev/null
 find . -type f -iname "*.bam" -exec samtools flagstat {} \; | sort
